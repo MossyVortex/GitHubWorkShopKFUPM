1164161804be1a0a57a753fd81ae4ac36e5b5394
MOSA ALSHAKHOURI <67518753+MossyVortex@users.noreply.github.com>
2023-02-11 18:30:12 +0300

    Update number_guessing_game.py

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,2 +1,21 @@
-def play_number_guessing_game():
-    pass
+# guessing game
+import random
+class number_guessing_game:
+
+    def number_guessing_game():
+
+        randomized = random.randint(0, 51) #Prints a random number between 3 and 7
+        print("The system generated a random number between 0 and 50")
+        flag = False
+        while not flag:
+            guess = int(input("try to guess the number: "))
+
+            if randomized == guess:
+                flag = True
+            elif randomized > guess:
+                print("Try a higher number")
+            else:
+                print("Try a smaller number")
+        print("YOU GOT IT! the number is " + str(randomized))
+
+    number_guessing_game()
