842c4926b70390f003b26a251661eddfe89eb64c
MOSA ALSHAKHOURI <67518753+MossyVortex@users.noreply.github.com>
2023-02-11 18:30:43 +0300

    Update rock_paper_scissor.py

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -1,3 +1,35 @@
-def play_rock_paper_scissors():
+import random
+class rock_paper_scissor:
+    
+    def play_rock_paper_scissors():
 
-    pass
+        chooser = ["rock", "paper", "scissors"]
+        computerChoice = random.choice(chooser) #Prints a random number between 3 and 7
+        print("The system generated a random chioce")
+
+        userChoice = int(input("Now it is your turn, enter the number you want \n1. Rock\n2. Paper\n3.Scissors\n"))
+
+        if userChoice == 1:
+            userChoice = "rock"
+        elif(userChoice == 2):
+            userChoice = "paper"
+        elif(userChoice == 3):
+            userChoice = "scissors"
+        else:
+            print("NOT ONE OF THE CHOICES!!!")
+
+        if(computerChoice == userChoice):
+            print("You both chose " + userChoice + " it's a tie!")
+        elif(computerChoice == "rock" and userChoice == "paper"):
+            print("Computer: " + computerChoice + " and user: " + userChoice + " user wins!")
+        elif(computerChoice == "rock" and userChoice == "scissors"):
+            print("Computer: " + computerChoice + " and user: " + userChoice + " comupter wins!")
+        elif(computerChoice == "paper" and userChoice == "rock"):
+            print("Computer: " + computerChoice + " and user: " + userChoice + " comupter wins!")
+        elif(computerChoice == "paper" and userChoice == "scissors"):
+            print("Computer: " + computerChoice + " and user: " + userChoice + " user wins!")
+        elif(computerChoice == "scissors" and userChoice == "rock"):
+            print("Computer: " + computerChoice + " and user: " + userChoice + " user wins!")
+        elif(computerChoice == "scissors" and userChoice == "paper"):
+            print("Computer: " + computerChoice + " and user: " + userChoice + " comupter wins!")
+    play_rock_paper_scissors()
